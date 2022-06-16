344f2b2dc0af8d052ddb1fcb8dfbd74097f74502
Betram Lalusha <betramlalusha@fb.com>
2022-06-16 12:02:01 -0700

    adding/updating of user profile pic added

--- app/src/main/java/com/example/instagramclone/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/instagramclone/Fragments/ProfileFragment.java
@@ -1,20 +1,28 @@
 package com.example.instagramclone.Fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.instagramclone.Adapters.PostsAdapter;
@@ -27,7 +35,9 @@ import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -36,11 +46,18 @@ public class ProfileFragment extends Fragment {
     TextView mUserName;
     TextView mUserName2;
     ImageView mUserPic;
+    ImageButton mChanePicButton;
     protected List<Post> mAllPosts;
     protected RecyclerView mRvPosts;
+    private File mPhotoFile;
+    ImageView mUploadedImage;
+    Button mSaveNewPicBtn;
+    public String mPhotoFileName = "photo.jpg";
     protected UserProfileAdapter mUserProfileAdapter;
     private SwipeRefreshLayout mSwipeContainer;
+    public final String APP_TAG = "InstagramClonePFFrag";
     protected EndlessRecyclerViewScrollListener mScrollListener;
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1044;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -65,6 +82,8 @@ public class ProfileFragment extends Fragment {
         mUserPic = view.findViewById(R.id.profilePicture);
         mUserName = view.findViewById(R.id.nameOfUser);
         mUserName2 = view.findViewById(R.id.nameOfUser2);
+        mChanePicButton = view.findViewById(R.id.changePicButton);
+        mSaveNewPicBtn = view.findViewById(R.id.saveNewButton);
         StaggeredGridLayoutManager gridLayoutManager =
                 new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
         mRvPosts = view.findViewById(R.id.userProfileRv);
@@ -96,6 +115,26 @@ public class ProfileFragment extends Fragment {
                 loadNextDataFromApi();
             }
         };
+
+        mChanePicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLaunchCamera(v);
+                mSaveNewPicBtn.setVisibility(View.VISIBLE);
+            }
+        });
+
+        mSaveNewPicBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mPhotoFile == null) {
+                    Toast.makeText(getContext(), "no image added", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                upDatePhoto(ParseUser.getCurrentUser(), mPhotoFile);
+                mSaveNewPicBtn.setVisibility(View.GONE);
+            }
+        });
 
         mRvPosts.addOnScrollListener(mScrollListener);
         mRvPosts.setAdapter(mUserProfileAdapter);
@@ -168,5 +207,61 @@ public class ProfileFragment extends Fragment {
         });
 
         mSwipeContainer.setRefreshing(false);
+    }
+
+    public void onLaunchCamera(View view) {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        mPhotoFile = getPhotoFileUri(mPhotoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", mPhotoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(APP_TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+    private void upDatePhoto(ParseUser currentUser, File photoFile) {
+        currentUser.put("profilePicture", new ParseFile(photoFile));
+        currentUser.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null) {
+                    Log.i(APP_TAG, "error occured changing picture " + e);
+                    Toast.makeText(getContext(), "error!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Toast.makeText(getContext(), "photo updated!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //change pic currently on display while it saves in background
+        mUserPic.setImageURI(Uri.parse(mPhotoFile.getPath()));
     }
 }
